2604b877f73d8c7ba1e60d4a60d7803c1d3e910a
Oleg <zazubik78@gmail.com>
2024-02-12 00:12:39 +0200

    add js for button

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ const mobilModalClose = document.querySelector('.header-btn-close');
 const mobilHeader = document.querySelector('.header-container');
 const mobilModalWrap = document.querySelector('.mobil-modal-header-wrap');
 const headerBtnOpen = document.querySelector('.open-menu-btn');
+const mobilAncor1 = document.querySelector('.mobil-ancor1');
+const mobilAncor2 = document.querySelector('.mobil-ancor2');
+const mobilAncor3 = document.querySelector('.mobil-ancor3');
 
 headerBtnOpen.addEventListener('click', function () {
   mobilHeader.classList.add('is-close');
@@ -15,3 +18,27 @@ mobilModalClose.addEventListener('click', function () {
 
   mobilModalWrap.classList.add('is-close');
 });
+mobilAncor1.addEventListener('click', function () {
+  mobilHeader.classList.remove('is-close');
+  mobilHeader.classList.add('is-open');
+
+  mobilModalWrap.classList.add('is-close');
+});
+mobilAncor2.addEventListener('click', function () {
+  mobilHeader.classList.remove('is-close');
+  mobilHeader.classList.add('is-open');
+
+  mobilModalWrap.classList.add('is-close');
+});
+mobilAncor3.addEventListener('click', function () {
+  mobilHeader.classList.remove('is-close');
+  mobilHeader.classList.add('is-open');
+
+  mobilModalWrap.classList.add('is-close');
+});
+
+ 
+
+ 
+ 
+
